7e61d1974f9d5aa4d1f6bdc2933cf16a86f3bc1f
Przemyslaw Zalewski <przemyslawwys@wp.pl>
2018-09-16 13:47:07 +0200

    Update project

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const burger = document.querySelector(".burger");
+const iconBars = document.querySelector(".fa-bars")
+const iconX = document.querySelector(".fa-times")
 const menu = document.querySelector("aside");
 
 burger.addEventListener("click", function () {
-    burger.classList.toggle("active");
+    iconBars.classList.toggle("show");
+    iconX.classList.toggle("show");
+    menu.classList.toggle("show");
 })
